acae6b0fdcb342a922832cdfad99c0a9827e50f7
SamuelBaran <samuel.baran@student.upjs.sk>
2020-12-13 00:27:26 +0100

    Zmena Rowmapper-a -> zmeny funkcii getAll, getById, getByVlastnikId

--- src/main/java/pozicovna/storage/MysqlNaradieDao.java
+++ src/main/java/pozicovna/storage/MysqlNaradieDao.java
@@ -12,67 +12,80 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
+import pozicovna.entities.Akcia;
 import pozicovna.entities.Naradie;
 import pozicovna.entities.Pouzivatel;
 
 public class MysqlNaradieDao implements NaradieDao {
 
 	private JdbcTemplate jdbcTemplate;
+	private PouzivatelDao pouzivatelDao;
+	private AkciaDao akciaDao;
 
 	public MysqlNaradieDao(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
+		this.pouzivatelDao = DaoFactory.INSTANCE.getPouzivatelDao();
+		this.akciaDao = DaoFactory.INSTANCE.getAkciaDao();
 	}
 
-//	private class NaradieRowMapper implements RowMapper<Naradie> {
-//		public Naradie mapRow(ResultSet rs, int rowNum) throws SQLException {
-//			Long id = rs.getLong("id");
-//			String znacka = rs.getString("znacka");
-//			String typ = rs.getString("typ");
-//			Boolean je_dostupne = rs.getBoolean("je_dostupne");
-//			Long druh_naradia_id = rs.getLong("druh_naradia_id");
-//			Long vlastnik_id = rs.getLong("vlastnik_id");
-//			String popis = rs.getString("popis");
-//
-//			return new Naradie(id, znacka, typ, je_dostupne, druh_naradia_id, vlastnik_id, popis);
-//		}
-//	}
+	private class NaradieRowMapper implements RowMapper<Naradie> {
+		public Naradie mapRow(ResultSet rs, int rowNum) throws SQLException {
+			Long id = rs.getLong("id");
+			String znacka = rs.getString("znacka");
+			String typ = rs.getString("typ");
+			Boolean jeDostupne = rs.getBoolean("je_dostupne");
+			String popis = rs.getString("popis");
+			String druhNaradia = rs.getString("druh_naradia");
+
+
+			Long vlastnikId = rs.getLong("vlastnik_id");
+			Pouzivatel vlastnik = pouzivatelDao.getById(vlastnikId);
+
+			List<Akcia> akcie = akciaDao.getByNaradieId(id);
+
+			return new Naradie(id, znacka, typ, jeDostupne, druhNaradia, vlastnik, popis, akcie);
+		}
+	}
 
 	public List<Naradie> getAll() {
-//		return jdbcTemplate.query(
-//				"SELECT id, znacka, typ, je_dostupne, druh_naradia_id, vlastnik_id, popis FROM naradie",
-//				new NaradieRowMapper());
-		return null;
+		String sql =
+				"SELECT n.id, dn.meno AS druh_naradia, n.znacka, n.typ, n.je_dostupne, n.popis, n.vlastnik_id " +
+				"FROM naradie AS n " +
+				"JOIN druh_naradia AS dn ON dn.id = n.druh_naradia_id ";
+		return jdbcTemplate.query(sql, new NaradieRowMapper());
 	}
 
 	public Naradie getById(long id) throws EntityNotFoundException {
-//		try {
-//			return jdbcTemplate.queryForObject(
-//					"SELECT id, znacky, typ, je_dostupne, druh_naradia_id, vlastnik_id, popis FROM naradie WHERE id = "
-//							+ id,
-//					new NaradieRowMapper());
-//		} catch (DataAccessException e) {
-//			throw new EntityNotFoundException("Náradie s id " + id + " not found");
-//		}
-		return null;
+		String sql =
+				"SELECT n.id, dn.meno AS druh_naradia, n.znacka, n.typ, n.je_dostupne, n.popis, n.vlastnik_id " +
+				"FROM naradie AS n " +
+				"JOIN druh_naradia AS dn ON dn.id = n.druh_naradia_id " +
+				"WHERE n.id = " + id;
+		try {
+			return jdbcTemplate.queryForObject(sql, new NaradieRowMapper());
+		} catch (DataAccessException e) {
+			throw new EntityNotFoundException("Náradie s id " + id + " not found");
+		}
 	}
 
 	public List<Naradie> getByVlastnikId(long id) throws EntityNotFoundException {
-//		try {
-//			return jdbcTemplate.query(
-//					"SELECT id, znacky, typ, je_dostupne, druh_naradia_id, vlastnik_id, popis FROM naradie WHERE vlastnik_id = "
-//							+ id,
-//					new NaradieRowMapper());
-//		} catch (DataAccessException e) {
-//			throw new EntityNotFoundException("Náradie vlastníka s id " + id + " not found");
-//		}
-		return null;
+		String sql =
+				"SELECT n.id, dn.meno AS druh_naradia, n.znacka, n.typ, n.je_dostupne, n.popis, n.vlastnik_id " +
+				"FROM naradie AS n " +
+				"JOIN druh_naradia AS dn ON dn.id = n.druh_naradia_id " +
+				"WHERE n.vlastnik_id = " + id;
+		try {
+			return jdbcTemplate.query(sql, new NaradieRowMapper());
+		} catch (DataAccessException e) {
+			throw new EntityNotFoundException("Náradie vlastníka s id " + id + " not found");
+		}
 	}
 
 	@Override
 	public Naradie save(Naradie naradie) throws EntityNotFoundException {
 //		try {
 //
-//			if (naradie.getId() == null) {// INSERT
+//			if (naradie.getId() == null) { // INSERT
 //				SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate);
 //				insert.withTableName("naradie");
 //				insert.usingGeneratedKeyColumns("id");
@@ -87,6 +100,8 @@ public class MysqlNaradieDao implements NaradieDao {
 //				valuesMap.put("je_dostupne", naradie.getJe_dostupne());
 //				valuesMapLong.put("druh_naradia_id", naradie.getDruh_naradia_id());
 //				valuesMapLong.put("vlastnik_id", naradie.getVlastnik_id());
+//
+//				// ToDo: riesim aj zmenu vlastnika???
 //
 //				insert.execute(valuesMapLong);
 //				return new Naradie(insert.executeAndReturnKey(valuesMap).longValue(), naradie.getZnacka(),
@@ -115,6 +130,7 @@ public class MysqlNaradieDao implements NaradieDao {
 	@Override
 	public Naradie delete(long id) throws EntityNotFoundException {
 		try {
+			//ToDo: treba mazat aj vsetky akcie
 			Naradie naradie = getById(id);
 			String sql = "DELETE FROM naradie WHERE id = " + id;
 			jdbcTemplate.update(sql);
