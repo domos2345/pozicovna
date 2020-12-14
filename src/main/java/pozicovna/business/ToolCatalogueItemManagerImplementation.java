214ca3a8723c438268a11594b1ff5d0e7b349fcd
Dominik <Dominik@DESKTOP-0HO24OQ>
2020-12-14 16:58:10 +0100

    ciastocne upravena save metoda v mysqlNaradieDao
    a popravde nič dokopy. pokus urobiť tableview pre myToolsScene

--- src/main/java/pozicovna/business/ToolCatalogueItemManagerImplementation.java
+++ src/main/java/pozicovna/business/ToolCatalogueItemManagerImplementation.java
@@ -1,21 +1,31 @@
 package pozicovna.business;
 
 import pozicovna.entities.Naradie;
+import pozicovna.entities.Pouzivatel;
 import pozicovna.storage.DaoFactory;
 import pozicovna.storage.NaradieDao;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ToolCatalogueItemManagerImplementation implements ToolCatalogueItemManager{
-    NaradieDao naradieDao = DaoFactory.INSTANCE.getNaradieDao();
+public class ToolCatalogueItemManagerImplementation implements ToolCatalogueItemManager {
+	NaradieDao naradieDao = DaoFactory.INSTANCE.getNaradieDao();
 
-    @Override
-    public List<ToolCatalogueItem> getToolCatalogueItems() {
-        List<Naradie> naradia = naradieDao.getAll();
-        List<ToolCatalogueItem> result = new ArrayList<>();
-        for(Naradie naradie: naradia)
-            result.add(new ToolCatalogueItem(naradie));
-        return result;
-    }
+	@Override
+	public List<ToolCatalogueItem> getToolCatalogueItems() {
+		List<Naradie> naradia = naradieDao.getAll();
+		List<ToolCatalogueItem> result = new ArrayList<>();
+		for (Naradie naradie : naradia)
+			result.add(new ToolCatalogueItem(naradie));
+		return result;
+	}
+
+	@Override
+	public List<ToolCatalogueItem> getOwnedToolCatalogueItems(Long vlastnikId) {
+		List<Naradie> naradia = naradieDao.getByVlastnikId(vlastnikId);
+		List<ToolCatalogueItem> result = new ArrayList<>();
+		for (Naradie naradie : naradia)
+			result.add(new ToolCatalogueItem(naradie));
+		return result;
+	}
 }
