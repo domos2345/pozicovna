1f1a247034d94fcc145b2303ca3a50d291b5b248
SamuelBaran <samuel.baran@student.upjs.sk>
2020-12-19 10:59:39 +0100

    GUI - Requests tabulka + buttony

--- src/main/java/pozicovna/gui/RequestsSceneController.java
+++ src/main/java/pozicovna/gui/RequestsSceneController.java
@@ -1,9 +1,30 @@
 package pozicovna.gui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import pozicovna.entities.Pouzivatel;
 
 public class RequestsSceneController extends LoggedInSceneController{
+    @FXML
+    private TableView<?> requestsTableView;
+    @FXML
+    private TableColumn<?, ?> druhColumn;
+    @FXML
+    private TableColumn<?, ?> znackaColumn;
+    @FXML
+    private TableColumn<?, ?> typColumn;
+    @FXML
+    private TableColumn<?, ?> ziadatelColumn;
+    @FXML
+    private TableColumn<?, ?> datumColumn;
+    @FXML
+    private Button rejectButton;
+    @FXML
+    private Button confirmButton;
+
 
     public RequestsSceneController(Pouzivatel pouzivatel) {
         super(pouzivatel);
@@ -13,5 +34,15 @@ public class RequestsSceneController extends LoggedInSceneController{
     void initialize() {
         super.initialize();
         requestsButton.setDisable(true);
+    }
+
+    @FXML
+    void confirmButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rejectButtonClick(ActionEvent event) {
+
     }
 }
